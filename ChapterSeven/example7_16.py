52f61a05a8b806fe44838ad4ce0eea96a2299e25
hsyy673150343 <13096337080@163.com>
2019-04-30 18:32:54 +0800

    更新

--- ChapterSeven/example7_16.py
+++ ChapterSeven/example7_16.py
@@ -18,7 +18,7 @@ def factorial(n):
     return 1 if n < 2 else n*factorial(n-1)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print('*' * 40, 'Calling snooze(.123)')
     snooze(.123)
     print('*' * 40, 'Calling factorial(6)')
